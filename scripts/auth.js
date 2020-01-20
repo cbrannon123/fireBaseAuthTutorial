46148445268ca415862e7375416798948d0c2672
Casey Brannon <cbrannon412@gmail.com>
2020-01-20 16:59:29 -0500

    user auth check for display and sec rules set

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,40 @@
+//set ruels for FB
+//listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //.onSnapshot sets up real time change
+        db.collection('guides').onSnapshot((snapshot) => {
+            setUpGuides(snapshot.docs);
+            setupUi(user)
+        }).catch(err => {
+            console.log(err.message)
+        });
+
+    } else {
+        setupUi();
+        setUpGuides([]);
+    }
+});
+
+//create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        //clost modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset()
+    }).catch(err => {
+        console.log(err.message)
+    })
+});
+
+
 //signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -8,7 +45,7 @@ signupForm.addEventListener('submit', (e) => {
     const password = signupForm['signup-password'].value;
 
     //signup user with asycn method
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {        
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset()
@@ -17,27 +54,21 @@ signupForm.addEventListener('submit', (e) => {
 
 //logout
 const logout = document.querySelector('#logout');
-
 logout.addEventListener('click', (e) => {
     e.preventDefault();
-    auth.signOut().then(() => {
-        console.log('user has signed out')
-    });
+    auth.signOut();
 });
 
 //signin
 
-const loginForm = document.querySelector('#logi`n-form');
-
+const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     //get user info
     const email = loginForm['login-email'].value;
     const password = loginForm['login-password'].value;
-
     auth.signInWithEmailAndPassword(email, password).then((cred) => {
-        console.log(cred.user)
 
         //close the login modal and reset form
         const modal = document.querySelector('#modal-login');
